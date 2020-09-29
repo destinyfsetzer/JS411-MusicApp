62a9908fe5d282dc2cdc50fba181911114fcb920
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2020-09-29 14:17:49 -0500

    states working

--- src/components/LoginComp.js
+++ src/components/LoginComp.js
@@ -1,27 +1,43 @@
-import React, { useState } from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Box from '@material-ui/core/Box'
-import DashboardComp from './DashboardComp'
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Box from "@material-ui/core/Box";
+import DashboardComp from "./DashboardComp";
 
-const LoginComp = ()  => {
-     const [isLoggedIn, toggleLogin] = useState(false)
-    if(!isLoggedIn){
+const LoginComp = () => {
+  const [isLoggedIn, toggleLogin] = useState(false);
+  if (!isLoggedIn) {
     return (
-        <div>
-      <Box component="span" m={30} display="flex" alignItems="center" justifyContent="center" flexDirection="column">
-      <TextField placeholder="Username" />
-      <TextField placeholder="Password" />
-      <Button variant="contained" onClick={() => toggleLogin(isLoggedIn ? false : true)} color="secondary">
-        Login
-      </Button>
-      </Box>
-        </div>
-    )} else {
+      <div>
+        <Box
+          component="span"
+          m={30}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          flexDirection="column"
+        >
+          <TextField placeholder="Username" />
+          <TextField placeholder="Password" />
+          <br />
+          <Button
+            variant="contained"
+            onClick={() => toggleLogin(isLoggedIn ? false : true)}
+            color="secondary"
+            size="medium"
+          >
+            Login
+          </Button>
+        </Box>
+      </div>
+    );
+  } else {
     return (
-    <div>
-    <DashboardComp />
-    </div> )}
-}
+      <div>
+        <DashboardComp />
+      </div>
+    );
+  }
+};
 
 export default LoginComp;
